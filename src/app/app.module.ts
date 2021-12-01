198c3cc95ee755ffbc68ce44f1332de770ffa788
Mrad-Hamza <hamza.mrad@esprit.tn>
2021-12-01 16:59:14 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { SideBarComponent } from './pages/side-bar/side-bar.component';
+import { PageContentComponent } from './pages/page-content/page-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SideBarComponent,
+    PageContentComponent
   ],
   imports: [
     BrowserModule,
